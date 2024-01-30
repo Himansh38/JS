af8869901440c4d01b7646275c3daddeed7f6ead
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2024-01-30 17:12:12 +0000

    conversion&operaiton

---
+++ 01_basics/03_conversion&operation.js
@@ -0,0 +1,68 @@
+let score = undefined//null//"22asdf"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(  valueInNumber)
+
+
+
+// "33" => 33
+// "33afa" => NaN (not an number)
+// true = 1/ false = 0
+
+let isLoggedIn = " kand"
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh " => true
+
+let someNumber = 33
+let stringNumber = String( someNumber)
+// console.log(typeof(stringNumber));
+
+/****************************Operations******************************** */
+
+let value =3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1= "hellow"
+let str2 = " himanshu"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" +2)
+// console.log(1 + "2");
+// console.log("1" +  2 + 2);//122  ??
+// console.log( 1 +  2 + "2");//32  ??
+ 
+// console.log(+true);
+// console.log(true+);
+
+let num1 , num2, num3 
+
+num1 = num2 = num3 = 2 + 2  //  tricky thing willnot considerable
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+// link to study =>
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
