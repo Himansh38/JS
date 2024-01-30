59e2ce1c7f18771614bcd447705677ecd0c9b38e
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2024-01-30 09:37:47 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144242
+let accountEmail= "himanshu@gmail.com"
+var accoutPassword = "12324"
+accountCity = " Santa"
+
+let accountState ;
+// accountId =2 not allowed becaude the ID is constant 
+accountEmail= "hg@gmail.com"
+accoutPassword = "98798"
+accountCity = "Sunnnnn"
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+console.log(accountId);
+console.table([accountEmail, accoutPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+// 48:00
+" use strict";//treat all js code as newer version 
+
+// alert(3+2);//we are using nodejs, not browser
+console.log(3
+    +
+     3)//code readability should be high 
+
+     
+console.log("himanshu")
+
+
+
+
+// number => 2 to power 53
+// bigint
+// srting => ""
+// boolean => true/false
+// null => standolone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
