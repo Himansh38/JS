960011a53a73434928c6f5ee92e38cd5562603cd
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2024-02-02 16:45:48 +0530

    comparisons

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -4,5 +4,16 @@
 // console.log(2 >= 1);
 // console.log(2 == 1);
 
-console.log("2" > 1 );
-console.log("02" > 1 );
+// console.log("2" > 1 );
+// console.log("02" > 1 );
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);// in line 7 to 12 to avoid the comparison for clean code 
+
+console.log(undefined == 0); // it give false in all case or  comparisons
+
+
+// ===
+
+
