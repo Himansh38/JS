e3895ed5a61e50011922478fa13082d006f8d097
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2024-03-21 06:43:45 +0000

    nums and math

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,24 +6,40 @@ const repoCount =  25
 console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`);
 
 // decalraiton
-const gameName  = new String ('Himan-sh')
+const gameName  = new String ('Himan-shu-gu-pta')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt('4'));
-console.log(gameName.indexOf('n'))
+// console.log(gameName.charAt('4'));
+// console.log(gameName.indexOf('n'))
 
 const newString = gameName.substring(0,4)
-console.log(newString);
+// console.log(newString);
 
 
 const anotherString = gameName.slice(-8, 4)  //confusion
-console.log(anotherString);
+// console.log(anotherString);
 
 
-const newStringOne = "   Himansh   "
+// const newStringOne = "   Himansh   "
 
-console.log(newStringOne.trim());//trim remove the spaces in the string 
+// console.log(newStringOne.trim());//trim remove the spaces in the string 
+
+const url = " https://hitesh.com/hitesh%20choudhary"
+
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes("hitesh"))
+console.log(url.includes("sundar"))
+
+
+console.log(gameName.split('-'));
+/*blink
+bold
+ charat
+  constrructor 
+ */
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const scroe = 400
+// console.log(scroe);
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNnumber = 34.5523
+// console.log(otherNnumber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())// that is us formate for viewing the number 
+// console.log(hundreds.toLocaleString('en-In'))// this is indian 
+
+// ++++++++++++++++++++++++++maths+++++++++++++++++++++++++
+ 
+// console.log(Math);
+
+// console.log(Math.abs(-4.9));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,5,67,6,3,33));
+// console.log(Math.max(2,5,67,6,3,33));
+
+console.log(Math.random());//the value is in btw 0-1
+console.log((Math.random()*10)+1); 
+console.log((Math.random()*10)+1); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
